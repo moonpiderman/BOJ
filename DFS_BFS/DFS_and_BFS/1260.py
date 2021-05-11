8631b4f0cbfd5f4282fe4c549ce1588f9669ff2e
moonpiderman <lotsofchuck@gmail.com>
2021-05-11 15:40:41 +0900

    re

---
+++ DFS_BFS/DFS_and_BFS/1260.py
@@ -0,0 +1,14 @@
+
+def solution(node, branch, point) :
+    answer = 0
+    n = node
+    b = branch
+    p = point
+
+    li = [n, b, p]
+    return li
+
+
+if __name__ == '__main__' :
+    node, branch, point = map(int, (input().split(' ')))
+    print(solution(node, branch, point))
