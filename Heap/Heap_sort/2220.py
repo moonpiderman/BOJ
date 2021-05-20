71aedcdd733b3391a10771b5baeaae4e5cfea9e9
moonpiderman <lotsofchuck@gmail.com>
2021-05-20 11:32:57 +0900

    heap

--- Heap/Heap_sort/2220.py
+++ Heap/Heap_sort/2220.py
@@ -1,10 +1,32 @@
-def solution(input) :
-    answer = []
-    swap = 0
+import heapq
 
-    return answer
+def swap(array, i, j) :
+    array[i], array[j] = array[j], array[i]
 
 if __name__ == '__main__' :
-    ip = 6
+    num = int(input())
 
-    solution(ip)
+    '''
+    array = [i for i in range(1, num + 1)]
+    heap = []
+
+    # 최소 swap으로 구현
+    for value in array :
+        heapq.heappush(heap, -value)
+
+    for i in range(len(heap)) :
+        print(-heap[i], end=' ')
+    '''
+
+    heap = [0, 1]
+    # 최대 힙으로 만들어가는 과정
+    for x in range(2, num + 1) :
+        heap.append(x)
+        swap(heap, x, x - 1)
+        y = x - 1
+        while y != 1 :
+            swap(heap, y, y//2)
+            y = y//2
+
+    for i in heap[1:] :
+        print(i, end=' ')
