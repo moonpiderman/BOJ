3abcf97e40402cd2dc74eddece56b041ef2d88ae
moonpiderman <lotsofchuck@gmail.com>
2021-07-27 20:26:39 +0900

    apartment

---
+++ Math/Apartment.py
@@ -0,0 +1,10 @@
+testCase = int(input())
+q = []
+for _ in range(testCase):
+    floor = int(input())
+    num = int(input())
+    f = [x for x in range(1, num + 1)]
+    for k in range(floor):
+        for i in range(1, num):
+            f[i] += f[i - 1]
+    print(f[-1])
