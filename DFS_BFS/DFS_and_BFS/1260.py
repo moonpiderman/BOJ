6f925f64fe3623c0e7a0a7b6f03d12cb4bacec57
moonpiderman <lotsofchuck@gmail.com>
2021-08-02 19:34:11 +0900

    fin

--- DFS_BFS/DFS_and_BFS/1260.py
+++ DFS_BFS/DFS_and_BFS/1260.py
@@ -1,14 +1,39 @@
+from collections import deque
+import sys
+read = sys.stdin.readline
 
-def solution(node, branch, point) :
-    answer = 1
-    n = node
-    b = branch
-    p = point
+def BFS(v):
+    q = deque()
+    q.append(v)
+    visit_list[v] = 1
+    while q:
+        v = q.popleft()
+        print(v, end=' ')
+        for i in range(1, n + 1):
+            if visit_list[i] == 0 and graph[v][i] == 1:
+                q.append(i)
+                visit_list[i] = 1
 
-    li = [n, b, p]
-    return li
+def DFS(v):
+    visit_list2[v] = 1
+    print(v, end=' ')
+    for i in range(1, n + 1):
+        if visit_list2[i] == 0 and graph[v][i] == 1:
+            DFS(i)
+# n : 정점의 갯수
+# m : 간선의 갯수
+# v : 탐색을 시작할 정점
+n, m, v = map(int, read().split())
 
+# 인덱스와 값을 일치시키기 위해서 n + 1 크기의 행렬을 만들어준다.
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+visit_list = [0] * (n + 1)
+visit_list2 = [0] * (n + 1)
 
-if __name__ == '__main__' :
-    node, branch, point = map(int, (input().split(' ')))
-    print(solution(node, branch, point))
+for _ in range(m):
+    a, b = map(int, read().split())
+    graph[a][b] = graph[b][a] = 1
+
+DFS(v)
+print()
+BFS(v)
