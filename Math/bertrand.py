888614f1a3e62472d90e056c1886e1edd23a4f8d
moonpiderman <lotsofchuck@gmail.com>
2021-08-31 21:43:08 +0900

    no

---
+++ Math/bertrand.py
@@ -0,0 +1,14 @@
+# 갯수 파악 함수
+def prime(n):
+    fact = [True] * n
+
+# 소수 파악 함수
+def check_prime()
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    else:
+        # 함수 시작띠
+        prime(n)
